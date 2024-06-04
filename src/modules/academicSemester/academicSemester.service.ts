41ee5ab029bae065e4af581332327ba154b00a70
Shariful4447 <shariful.islam4447@gmail.com>
2024-06-04 02:31:30 +0200

    Envalid semsester code added if semster code doesnot matched with defined code

--- src/modules/academicSemester/academicSemester.service.ts
+++ src/modules/academicSemester/academicSemester.service.ts
@@ -2,6 +2,18 @@ import { TAcademicSemester } from "./academicSemester.interface";
 import { AcademicSemester } from "./academicSemester.model";
 
 const createAcademicSemesterIntoDB = async (payload: TAcademicSemester) => {
+  //check semester name and code are same
+  type TacademicSemesterCodeMapper = {
+    [key: string]: string;
+  };
+  const academicSemesterCodeMapper: TacademicSemesterCodeMapper = {
+    Autumn: "01",
+    Summer: "02",
+    Fall: "03",
+  };
+  if (academicSemesterCodeMapper[payload.name] !== payload.code) {
+    throw new Error("Envalid semster code");
+  }
   const result = await AcademicSemester.create(payload);
   return result;
 };
