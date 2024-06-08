73f7c6a7def976101dc7a4a64241ae9429acc343
Shariful4447 <shariful.islam4447@gmail.com>
2024-06-08 23:46:56 +0200

    stusent registration process of unique semesterce and year

--- src/modules/user/user.utils.ts
+++ src/modules/user/user.utils.ts
@@ -21,7 +21,20 @@ const findLastStudentId = async () => {
 // first student id will be 00000
 // id format should be year+semester-code+initial 4 digit number+1
 export const generateStudentId = async (payload: TAcademicSemester) => {
-  const currentId = (await findLastStudentId()) || (0).toString();
+  let currentId = (0).toString();
+  const lastStudentId = await findLastStudentId();
+  const lastStudentSemesterCode = lastStudentId?.substring(4, 6);
+  const lastStudentYear = lastStudentId?.substring(0, 4);
+  const currentSemesterCode = payload.code;
+  const currentYear = payload.year;
+
+  if (
+    lastStudentId &&
+    lastStudentSemesterCode === currentSemesterCode &&
+    lastStudentYear === currentYear
+  ) {
+    currentId = lastStudentId.substring(6);
+  }
   let incrementId = (Number(currentId) + 1).toString().padStart(4, "0");
   incrementId = `${payload.year}${payload.code}${incrementId}`;
   return incrementId;
