5e8584b39067094c24db1ed63a58489939c1e219
Shariful4447 <shariful.islam4447@gmail.com>
2024-06-11 09:45:03 +0200

    academic departmnet validation with pr hook middleware for update function added

--- src/modules/academicDepartment/academicDepartment.model.ts
+++ src/modules/academicDepartment/academicDepartment.model.ts
@@ -28,6 +28,18 @@ academicDepartmentSchema.pre("save", async function (next) {
     throw new Error("This Department already exists!");
   }
 });
+
+// check id is available for update
+
+academicDepartmentSchema.pre("findOneAndUpdate", async function (next) {
+  const query = this.getQuery();
+  const isDepartmentExist = await AcademicDepartment.findOne(query);
+  if (!isDepartmentExist) {
+    throw new Error("this department doesnot exists");
+  }
+  next();
+});
+
 export const AcademicDepartment = model<TAcademicDepartment>(
   "AcademicDepartment",
   academicDepartmentSchema
