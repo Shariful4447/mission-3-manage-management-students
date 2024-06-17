74460dca9916bc104ed3906ee8a220cb0e5e4630
Shariful4447 <shariful.islam4447@gmail.com>
2024-06-17 23:26:10 +0200

    global error file change in order to format the error response

--- src/middlewares/globalErrorHandlers.ts
+++ src/middlewares/globalErrorHandlers.ts
@@ -1,19 +1,58 @@
-import { NextFunction, Request, Response } from "express";
+import { ErrorRequestHandler, NextFunction, Request, Response } from "express";
+import { ZodError, ZodIssue } from "zod";
+import { TErrorSource } from "../app/interface/error";
 
-const globalErrorHandler = (
-  err: any,
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  const statusCode = err.statusCode || 500;
-  const message = err.message || "Something went wrong!";
+const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  // setting default value
+  let statusCode = err.statusCode || 500;
+  let message = err.message || "Something went wrong!";
+
+  let errorSources: TErrorSource = [
+    {
+      path: "",
+      message: "Something went wrong!",
+    },
+  ];
+  const handleZodError = (err: ZodError) => {
+    const errorSources: TErrorSource = err.issues.map((issue: ZodIssue) => {
+      return {
+        path: issue?.path[issue.path.length - 1],
+        message: issue.message,
+      };
+    });
+    const statusCode = 400;
+    return {
+      statusCode,
+      message: "Zod Validation Error",
+      errorSources,
+    };
+  };
+
+  if (err instanceof ZodError) {
+    const simplifiedError = handleZodError(err);
+    statusCode = simplifiedError?.statusCode;
+    (message = simplifiedError?.message),
+      (errorSources = simplifiedError?.errorSources);
+  }
 
   return res.status(statusCode).json({
     success: false,
     message,
-    error: err,
+    errorSources,
+    // error: err,
   });
 };
 
 export default globalErrorHandler;
+
+/*
+want to error pattern like this:
+success
+message:
+errorSources[
+  path:'',
+  message: ''
+
+]
+stack:
+*/
