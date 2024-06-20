5e8282a7fe8d8359e0e2d13060b8e05d15a12309
Shariful4447 <shariful.islam4447@gmail.com>
2024-06-20 17:42:15 +0200

    interface and model for course done

---
+++ src/modules/course/course.model.ts
@@ -0,0 +1,38 @@
+import { Schema, model } from "mongoose";
+import { TCourse, TPreRequisiteCourses } from "./course.interface";
+
+const preRequisitecoursesSchema = new Schema<TPreRequisiteCourses>({
+  course: {
+    type: Schema.Types.ObjectId,
+  },
+  isDeleted: {
+    type: Boolean,
+    default: false,
+  },
+});
+const courseSchema = new Schema<TCourse>({
+  title: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+  },
+  prefix: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  code: {
+    type: Number,
+    required: true,
+    trim: true,
+  },
+  credits: {
+    type: Number,
+    required: true,
+    trim: true,
+  },
+  preRequisiteCourses: [preRequisitecoursesSchema],
+});
+
+const Course = model<TCourse>("Course", courseSchema);
