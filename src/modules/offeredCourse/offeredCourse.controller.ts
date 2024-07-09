127d9c75618e89fc22b8e063fc8d356b13f11276
Shariful4447 <shariful.islam4447@gmail.com>
2024-07-09 21:10:57 +0200

    offered course created succesfully

---
+++ src/modules/offeredCourse/offeredCourse.controller.ts
@@ -0,0 +1,17 @@
+import httpStatus from "http-status";
+import catchAsync from "../../utils/catchAsync";
+import sendResponse from "../../utils/sendResponse";
+import { OfferedCourseService } from "./offeredCourse.service";
+
+const createOfferedCourse = catchAsync(async (req, res) => {
+  const result = await OfferedCourseService.createOfferedCourseIntoDB(req.body);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Course is created succesfully",
+    data: result,
+  });
+});
+export const OfferedCourseController = {
+  createOfferedCourse,
+};
--- src/modules/offeredCourse/offeredCourse.route.ts
+++ src/modules/offeredCourse/offeredCourse.route.ts
@@ -1,6 +1,14 @@
 import express from "express";
 import validateRequest from "../../middlewares/validateRequest";
+import { OfferedCourseValidations } from "./offeredCourse.validation";
+import { OfferedCourseController } from "./offeredCourse.controller";
 
 const router = express.Router();
+
+router.post(
+  "/create-offered-course",
+  validateRequest(OfferedCourseValidations.createOfferedCourseValidationSchema),
+  OfferedCourseController.createOfferedCourse
+);
 
 export const OfferedCourseRoutes = router;
