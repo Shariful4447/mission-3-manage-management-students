255eea09b2ec695008935d713b8d0be1b702adca
Shariful4447 <shariful.islam4447@gmail.com>
2024-07-10 04:27:18 +0200

    check if the semester registrtion is exist or not

--- src/modules/offeredCourse/offeredCourse.service.ts
+++ src/modules/offeredCourse/offeredCourse.service.ts
@@ -1,7 +1,24 @@
+import httpStatus from "http-status";
+import AppError from "../../app/errors/AppError";
+import { SemesterRegistration } from "../semesterRegistration/semesterRegistration.model";
 import { TOfferedCourse } from "./offeredCourse.interface";
 import { OfferedCourse } from "./offeredCourse.model";
 
 const createOfferedCourseIntoDB = async (payload: TOfferedCourse) => {
+  const {
+    semesterRegistration,
+    academicFaculty,
+    academicDepartment,
+    course,
+    faculty,
+  } = payload;
+  // check if the semester registration is exists
+  const isSemesterRegistrationExists = await SemesterRegistration.findById(
+    semesterRegistration
+  );
+  if (!isSemesterRegistrationExists) {
+    throw new AppError(httpStatus.NOT_FOUND, "Semester registration not found");
+  }
   const result = await OfferedCourse.create(payload);
   return result;
 };
