cd8f9c2bae16b3b1e58be403aa85a3b2a7847a49
Shariful4447 <shariful.islam4447@gmail.com>
2024-07-13 23:51:02 +0200

    offred course start time time conflict resolve

--- src/modules/offeredCourse/offeredCourse.service.ts
+++ src/modules/offeredCourse/offeredCourse.service.ts
@@ -94,6 +94,15 @@ const createOfferedCourseIntoDB = async (payload: TOfferedCourse) => {
 
   assignSchedulesOfFaculties.forEach((schedule) => {
     const existingStartTime = new Date(`1970-01-01T${schedule.startTime}`);
+    const existingEndTime = new Date(`1970-01-01T${schedule.endTime}`);
+    const newStartTime = new Date(`1970-01-01T${newSchedule.startTime}`);
+    const newEndTime = new Date(`1970-01-01T${newSchedule.endTime}`);
+    if (newStartTime < existingEndTime && newEndTime > newStartTime) {
+      throw new AppError(
+        httpStatus.BAD_REQUEST,
+        "startTime and endTime conflict !! This faculty is not availbale on the mentioned day and time "
+      );
+    }
   });
   const result = await OfferedCourse.create({ ...payload, academicSemester });
   return result;
