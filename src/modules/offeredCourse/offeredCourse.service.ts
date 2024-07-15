2ed4e5528897fade6a15c527c5d7471eda3cc611
Shariful4447 <shariful.islam4447@gmail.com>
2024-07-15 23:13:20 +0200

    error in time schedules

--- src/modules/offeredCourse/offeredCourse.service.ts
+++ src/modules/offeredCourse/offeredCourse.service.ts
@@ -7,6 +7,7 @@ import { AcademicFaculty } from "../academicFaculty/academicFaculty.model";
 import { AcademicDepartment } from "../academicDepartment/academicDepartment.model";
 import { Course } from "../course/course.model";
 import { Faculty } from "../Faculty/faculty.model";
+import { hasTimeConflict } from "./offeredCourse.utils";
 
 const createOfferedCourseIntoDB = async (payload: TOfferedCourse) => {
   const {
@@ -91,19 +92,26 @@ const createOfferedCourseIntoDB = async (payload: TOfferedCourse) => {
     startTime,
     endTime,
   };
+  if (hasTimeConflict(assignSchedules, newSchedule)) {
+    throw new AppError(
+      httpStatus.BAD_REQUEST,
+      "startTime and endTime conflict !! This faculty is not availbale on the mentioned day and time "
+    );
+  }
 
-  assignSchedules.forEach((schedule) => {
-    const existingStartTime = new Date(`1970-01-01T${schedule.startTime}`);
-    const existingEndTime = new Date(`1970-01-01T${schedule.endTime}`);
-    const newStartTime = new Date(`1970-01-01T${newSchedule.startTime}`);
-    const newEndTime = new Date(`1970-01-01T${newSchedule.endTime}`);
-    if (newStartTime < existingEndTime && newEndTime > existingStartTime) {
-      throw new AppError(
-        httpStatus.BAD_REQUEST,
-        "startTime and endTime conflict !! This faculty is not availbale on the mentioned day and time "
-      );
-    }
-  });
+  //   assignSchedules.forEach((schedule) => {
+  //     const existingStartTime = new Date(`1970-01-01T${schedule.startTime}`);
+  //     const existingEndTime = new Date(`1970-01-01T${schedule.endTime}`);
+  //     const newStartTime = new Date(`1970-01-01T${newSchedule.startTime}`);
+  //     const newEndTime = new Date(`1970-01-01T${newSchedule.endTime}`);
+  //     if (newStartTime < existingEndTime && newEndTime > existingStartTime) {
+  //       throw new AppError(
+  //         httpStatus.BAD_REQUEST,
+  //         "startTime and endTime conflict !! This faculty is not availbale on the mentioned day and time "
+  //       );
+  //     }
+  //   });
+
   const result = await OfferedCourse.create({ ...payload, academicSemester });
   return result;
 };
--- src/modules/offeredCourse/offeredCourse.utils.ts
+++ src/modules/offeredCourse/offeredCourse.utils.ts
@@ -1,19 +1,17 @@
 import { TDays, TSchedule } from "./offeredCourse.interface";
 
-const hasTimeConflict = (
+export const hasTimeConflict = (
   assignSchedules: TSchedule[],
   newSchedule: TSchedule
 ) => {
-  assignSchedules.forEach((schedule) => {
+  for (const schedule of assignSchedules) {
     const existingStartTime = new Date(`1970-01-01T${schedule.startTime}`);
     const existingEndTime = new Date(`1970-01-01T${schedule.endTime}`);
     const newStartTime = new Date(`1970-01-01T${newSchedule.startTime}`);
     const newEndTime = new Date(`1970-01-01T${newSchedule.endTime}`);
     if (newStartTime < existingEndTime && newEndTime > existingStartTime) {
-      throw new AppError(
-        httpStatus.BAD_REQUEST,
-        "startTime and endTime conflict !! This faculty is not availbale on the mentioned day and time "
-      );
+      return true;
     }
-  });
+  }
+  return false;
 };
