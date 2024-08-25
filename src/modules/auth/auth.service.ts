4a1e0d8741f9256ce11fbc121c1882c3d5bca2f8
Shariful4447 <shariful.islam4447@gmail.com>
2024-08-25 03:25:40 +0200

    check user validation before login

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -4,12 +4,26 @@ import { User } from "../user/user.model";
 import { TLoginUser } from "./auth.interface";
 
 const loginUser = async (payload: TLoginUser) => {
-  // check if the user is presend
+  // check if the user is present
 
   const isUserPresent = await User.findOne({ id: payload?.id });
   if (!isUserPresent) {
     throw new AppError(httpStatus.NOT_FOUND, "user not found");
   }
+  //   check is the user is deleted
 
-  return result;
+  const isUserDeleted = isUserPresent?.isDeleted;
+  if (isUserDeleted) {
+    throw new AppError(httpStatus.FORBIDDEN, "user already deleted");
+  }
+  //   check is the user status blocked or in -prgress
+
+  const userStatus = isUserPresent?.status;
+  if (userStatus === "blocked") {
+    throw new AppError(
+      httpStatus.FORBIDDEN,
+      "user already status not permited, user is blocked"
+    );
+  }
+  return {};
 };
